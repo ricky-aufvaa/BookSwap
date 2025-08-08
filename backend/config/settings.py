ad67f768f81516c83bf10e5d474d5dff1b78a481
ricky-aufvaa <aufvaa@gmail.com>
2025-08-08 14:27:33 +0530

    docker added

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     GOOGLE_BOOKS_API_KEY: str ="AIzaSyBX9ShEmKc1rMz_VF_mH-tTYPrJrpHdguU"
     # DATABASE_URL: str = "postgresql+asyncpg://postgress:Welcome@123456@localhost:5432/QuizApplication"
-    DATABASE_URL: str = 'postgresql+asyncpg://postgress:Welcome%40123456@localhost:5432/QuizApplication'
+    DATABASE_URL: str = 'postgresql+asyncpg://postgress:Welcome%40123456@db:5432/QuizApplication'
     SECRET_KEY: str = "Welcome@123456"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
