3c54d64b855da11e72078901d6f7ef67b20feb90
ricky-aufvaa <aufvaa@gmail.com>
2025-08-19 23:39:24 +0530

    debugging city users

--- backend/api/books.py
+++ backend/api/books.py
@@ -52,10 +52,10 @@ async def search_books(query: str, db: AsyncSession = Depends(get_db), current_u
     city_users = await db.execute(select(User.id).where(User.city == current_user.city, User.username != current_user.username))
     print(f"city_users {city_users}")
     try:
-        city_user_ids = [u.id for u in city_users.scalers().all()]
+        city_user_ids = [u.id for u in city_users.scalars().all()]
     except:
     # if not city_user_ids:
-        print(f"city users are {city_users.scalers().all()}")
+        print(f"city users are {city_users.scalars().all()}")
         return google_books
     
     
